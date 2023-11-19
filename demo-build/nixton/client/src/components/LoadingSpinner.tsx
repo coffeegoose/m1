dcc45bc1446e707042fb3916eae19977e9625dd9
coffeegoose <129652526+coffeegoose@users.noreply.github.com>
2023-11-19 09:41:15 +0000

    Delete files

--- demo-build/nixton/client/src/components/LoadingSpinner.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { LoadingSpinnerStyles } from '../styles/LoadingSpinnerStyles'
-
-export const LoadingSpinner: React.FC = () => (
-  <LoadingSpinnerStyles>
-    <div className="double-bounce1"></div>
-    <div className="double-bounce2"></div>
-  </LoadingSpinnerStyles>
-)
--- demo-build/nixton/client/src/helpers/modalTypes.ts
+++
@@ -1,5 +0,0 @@
-export enum ModalTypes {
-    settings,
-    addTask,
-    addWebsite
-}
